74c9e70ae87178b80c728bdc5d6b2d7d6bcd3ad8
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2022-04-20 14:14:10 +0800

    更新提醒

--- meituan.py
+++ meituan.py
@@ -25,10 +25,10 @@ def play_voice(content):
     threading.Thread(target=playsound, args=(video_path,)).start()
 
 def click_btn(d, text):
-    if text in ["确认并支付", "立即支付"]:
-        play_voice("success")
     if d(textContains=text).exists:
         print("点击" + text)
+        if text in ["确认并支付", "立即支付"]:
+            play_voice("success")
         d(textContains=text).click()
 
 def qiang_cai(device_name):
